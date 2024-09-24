8c9ce46fadc68cee484be649cc046a0014b30ac8
Indraneel Vairagare <indraneel207@gmail.com>
2024-09-24 10:59:14 -0700

    Added tests dummy file

---
+++ tests/test_capture.py
@@ -0,0 +1,10 @@
+"""
+This module contains the unit tests for the Capture class in the capture module.
+"""
+
+import unittest
+
+class TestCapture(unittest.TestCase):
+    """
+    Test the Capture class methods.
+    """
