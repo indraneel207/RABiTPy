f7fe1d0b3e6ae2d193e6590af8c0d1c156c02a1d
Indraneel Vairagare <indraneel207@gmail.com>
2024-12-16 22:30:54 -0500

    Bump version to 1.0.1 in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='RABiTPy',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=[
         # Add your package dependencies here
